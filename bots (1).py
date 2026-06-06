87e820c16dbee8b97541b9b348986b3ffe8f9312
smajne789-jpg <smajne789@gmail.com>
2026-06-06 06:11:00 +0300

    Update bots (1).py

--- bots (1).py
+++ bots (1).py
@@ -68,12 +68,13 @@ async def process_giveaway_title(message: types.Message):
    msg = await bot.send_photo(
     CHANNEL_ID,
     photo=GIVEAWAY_PHOTO,
-    caption=(
-        f"<b>🎁 МИНИ-ИГРА ОТ ИЛЮШКИ НАЧАЛАСЬ</b>\n\n"
-        f"🏆 <b>ПРИЗ:</b> {giveaway_title}\n\n"
-        f"👉 <b>УЧАСТВОВАТЬ ТУТ @brazers_promo</b>\n\n"
-        f"😈 <b>МИНИ-ИЛЮШКИ ({len(participants)}/{MAX_PARTICIPANTS}):</b>\n"
-        f"(пусто)"
+           caption=(
+            f"<b>🎁 МИНИ-ИГРА ОТ ИЛЮШКИ НАЧАЛАСЬ</b>\n\n"
+            f"🏆 <b>ПРИЗ:</b> {giveaway_title}\n\n"
+            f"👉 <b>УЧАСТВОВАТЬ ТУТ @brazers_promo</b>\n\n"
+            f"😈 <b>МИНИ-ИЛЮШКИ (0/{MAX_PARTICIPANTS}):</b>\n"
+            f"(пусто)"
+        ),
     ),
     reply_markup=join_keyboard(True),
     parse_mode="HTML"
